69c669f086519a1c6be5f405136874b7c07b4002
dvt <trinhvideo123@gmail.com>
2022-06-30 15:51:42 +0700

    add terms and policy

--- src/layout/AppFooter.tsx
+++ src/layout/AppFooter.tsx
@@ -2,35 +2,38 @@ import React from "react";
 import {Divider, Stack, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 
-const AppFooter = () => {
-    const footerText = ["Gallery", "About Us", "Contact Us"];
-    const footerLink = ["/gallery", "#", "#"];
+const FooterText = ({children}: {children: React.ReactNode}) => (
+    <Typography variant="body2" align="center" color="textSecondary">
+        {children}
+    </Typography>
+);
 
-    return (
-        <footer className="my-2 ml-2">
-            <Stack
-                direction="row"
-                divider={<Divider orientation="vertical" flexItem />}
-                spacing={1}
+const AppFooter = () => (
+    <footer className="my-2 ml-2">
+        <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" flexItem />}
+            spacing={1}
+        >
+            <Link to="/gallery" className="no-underline">
+                <FooterText>Gallery</FooterText>
+            </Link>
+            <Link
+                to="/terms_and_conditions.txt"
+                target="_blank"
+                className="no-underline"
             >
-                {footerText.map((label, index) => (
-                    <Link
-                        to={footerLink[index]}
-                        key={label}
-                        className="no-underline"
-                    >
-                        <Typography
-                            variant="body1"
-                            align="center"
-                            color="textSecondary"
-                        >
-                            {label}
-                        </Typography>
-                    </Link>
-                ))}
-            </Stack>
-        </footer>
-    );
-};
+                <FooterText>Terms</FooterText>
+            </Link>
+            <Link
+                to="/privacy_policy.txt"
+                target="_blank"
+                className="no-underline"
+            >
+                <FooterText>Privacy Policy</FooterText>
+            </Link>
+        </Stack>
+    </footer>
+);
 
 export default AppFooter;
