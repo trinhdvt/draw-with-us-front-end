f19c9d899ad5f072654526dc6c321680c2626cfb
dvt <trinhvideo123@gmail.com>
2022-07-15 14:31:35 +0700

    add i18n for Paused Screen

--- src/pages/game-play/components/WaitingScreen.tsx
+++ src/pages/game-play/components/WaitingScreen.tsx
@@ -50,7 +50,9 @@ const WaitNextTurn = () => (
     <WaitingScreen title={i18n.t("game_play.waiting_screen.wait_next_turn")} />
 );
 
-const PausedGame = () => <WaitingScreen title={"The game has been paused"} />;
+const PausedGame = () => (
+    <WaitingScreen title={i18n.t("game_play.waiting_screen.paused")} />
+);
 
 const ReadyToStartGame = () => {
     const {t} = useTranslation();
@@ -64,8 +66,8 @@ const ReadyToStartGame = () => {
                     {t("game_play.start_the_game")}
                 </Button>
                 <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75" />
+                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500" />
                 </span>
             </span>
         </WaitingScreen>
